e6c8bc6763ff3e36476fe646dab78506c0e6bb93
Dias <manolo.dias.junior@gmail.com>
2024-06-18 12:24:43 -0300

    build: configuring dependencies

---
+++ webpack.mix.js
@@ -0,0 +1,12 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/dashboard.js', 'public/js')
+    .js('resources/js/customer.js', 'public/js')
+    .js('resources/js/product.js', 'public/js')
+    .postCss('resources/css/styles.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+    .babelConfig({
+        presets: ['@babel/preset-env'],
+    })
+    .sourceMaps();
